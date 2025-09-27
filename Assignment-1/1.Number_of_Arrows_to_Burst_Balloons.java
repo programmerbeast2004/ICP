e466028b32204adbbf749c42bcbe23384d616788
Apoorv <codeguy2024@gmail.com>
2025-09-27 18:13:27 +0530

    added solutions for assignment 1

---
+++ Assignment-1/1.Number_of_Arrows_to_Burst_Balloons.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findMinArrowShots(int[][] points) {
+        Arrays.sort(points,(a,b)->Integer.compare(a[1],b[1]));
+        int min=points[0][1];
+        int arrows=1;
+        for(int i=1;i<points.length;i++)
+        {
+            if(points[i][0]<=min)
+            {
+                continue;
+            }
+            else{
+                min=points[i][1];
+                arrows++;
+            }
+        }
+        return arrows;        
+    }
+}
